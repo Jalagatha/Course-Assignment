8fb7e2fb65642987729c65116ff46c8dae35d96e
Mugisha Ivan Jalagatha <jalagatha@gmail.com>
2024-03-15 02:10:10 +0300

    -Modified to public folder

--- app.js
+++ app.js
@@ -2,49 +2,58 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import morgan from 'morgan';
-import bodyParser from 'body-parser';
 
 
-const users = [
-    { username: 'kcf1', password: 'kcf11234' },
-    { username: 'kcf2', password: 'kcf21234' },
+let isLoggedIn=false
+const Port=4000||process.env.PORT ;
+let users = [
+    { username: 'ivan', password: '1234' },
+    { username: 'ivan2', password: '1234' }
 
 ];
 const app = express();
-const PORT = 4000;
+
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(morgan('dev')); 
 
-app.get('/', (req, res) => {
+app.get('/login', (req, res) => {
    res.sendFile(path.join(__dirname,'public','login.html'));
 
 });
 
 
 app.post('/login', (req, res) => {
-    const username = req.body.password;
-    const password = req.body.password
-
+    const {username,password} = req.body;
     const user = users.find(u => u.username === username && u.password === password);
     if (user) {
-        res.redirect('/courses');
+        isLoggedIn = true;
+        res.redirect('/node-course');
     } else {
-        res.redirect('/login');
+        res.send('Invalid username or password.');
     }
 });
 
 
-app.get('/courses', (req, res) => {
+app.get('/node-course', (req, res) => {
 
-    res.sendFile(path.join(__dirname,'public','node-course.html'));
+    const user = isLoggedIn;
+
+    if (user) {
+        
+        res.sendFile(path.join(__dirname,'public','node-course.html'));
+    } else {
+        res.send('You Need to Login First');
+    }
+
+    
 });
 
 
 // Start the server
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port "http://localhost:4000"`);
+app.listen(Port, () => {
+  console.log(`Server is running on port "http://localhost:${Port}"`);
 });
